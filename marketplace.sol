be5b382e5c9b98c46d1038248816037b9cf9c369
Remix Workspace <>
2022-07-31 18:45:31 +0530

    Marketplace

---
+++ marketplace.sol
@@ -0,0 +1,37 @@
+pragma solidity >0.7.0 <0.8.0;
+
+contract Marketplace {
+    address public seller;
+    address public buyer;
+    mapping (address => uint) public balances;
+
+    event ListItem(address seller, uint price);
+    event PurchasedItem(address seller, address buyer, uint price);
+
+    enum StateType {
+          ItemAvailable,
+          ItemPurchased
+    }
+
+    StateType public State;
+
+    constructor() public {
+        seller = msg.sender;
+        State = StateType.ItemAvailable;
+    }
+    
+    function initialBal(address participant, uint amount) public{
+        require(msg.sender == participant,"You cannot update someone else balance");
+        require(balances[participant] == 0,"You cannot add more funds");
+        balances[participant] += amount;
+    }
+
+    function buy(address seller, address buyer, uint price) public payable {
+        require(price <= balances[buyer], "Insufficient balance");
+        State = StateType.ItemPurchased;
+        balances[buyer] -= price;
+        balances[seller] += price;
+
+        emit PurchasedItem(seller, buyer, msg.value);
+    }
+}
